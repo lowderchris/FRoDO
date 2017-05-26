7f2ff3950d89f0f95dcce9d81ab2d74eff2ae5df
Chris Lowder <lowder.chris@icloud.com>
2017-05-26 18:26:12 +0100

    Created BRIEF outline of FRoDO.py

---
+++ FRoDO.py
@@ -0,0 +1,21 @@
+# FRoDO.py
+# Flux Rope Detection and Observation
+
+# Import libraries
+
+# Read a few user parameters from file
+# Include here data/output directories
+
+# Work through all of the nasty date calculations
+
+# Define any other variables needed
+
+# Begin cycling through time frames
+
+	# Diagnostic readouts!
+
+	# Here there be flux rope detection
+
+	# Output appropriate arrays to disk
+
+# Output any completed time-series variables
