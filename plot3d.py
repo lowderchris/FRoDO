acf4e20b66d5c8e01ad75ca8f3215f1cfaf3943b
Chris Lowder <lowder.chris@icloud.com>
2017-08-16 15:25:49 +0100

    Added plot3d framework

--- plot3d.py
+++ plot3d.py
@@ -1,6 +1,11 @@
 # plot3d.py
 # Set of routines to plot FRoDO output data in
 # the third dimension.
+'''
+This set of modules works alongside generated data from the FRoDO code.
+Specify plotting parameters within the config.cfg file.
+For all additional details, consult the aptly named README file.
+'''
 
 ## To run headlessly in the background,
 ## xvfb-run --server-args="-screen 0 1024x768x24" python plot3d.py
@@ -8,6 +13,7 @@
 # Import libraries
 import os
 import glob
+import re
 from datetime import datetime
 import matplotlib
 from matplotlib import cm, colors
@@ -25,6 +31,11 @@ import configparser
 config = configparser.ConfigParser()
 config.read('config.cfg')
 
+datdir = config['paths']['datdir']
+bdatprefix = config['paths']['bdatprefix']
+adatprefix = config['paths']['adatprefix']
+outdir = config['paths']['outdir']
+
 frmdir = config['paths']['frmdir']
 movname = config['paths']['movname']
 
@@ -40,5 +51,44 @@ vr1 = config['plot3d']['vr1']
 frmrate = config['plot3d']['frmrate']
 
 def plot3d():
+    '''
+    Uses specified plotting parameters to animate a three-dimension visualization of magnetic flux rope evolution.
+    '''
 
-    
+    # Generate a list of files for animating
+    files = glob.glob(outdir + 'fr-'+'*.nc')
+    files.sort()
+
+    # Read radial extent and duration filtered index
+    infile = open(outdir + '/hist/fr-frg.pkl', 'rb')
+    fr_frg = pickle.load(infile)
+    infile.close()
+    fr_frg = fr_frg.astype(np.int)
+
+    # Iterate through these files
+    for file in files:
+        csfrm = re.split('fr-|\.', file)[1]
+
+        # Read required data
+        b = b_sim_netcdf.SphB_sim(datdir+bdatprefix+csfrm+'.nc', datdir+adatprefix+csfrm+'.nc', 128, 128, 128)
+        f = netcdf.netcdf_file(outdir + 'fr-'+csfrm+'.nc', 'r')
+        frmap = f.variables['frmap'][:,:].copy()
+        br0 = f.variables['br0'][:,:].copy()
+        frhlcy = f.variables['frhlcy'][:,:].copy()
+        lat = f.variables['lat'][:].copy()
+        lon = f.variables['lon'][:].copy()
+        f.close()
+
+        # Create array of filtered flux rope footprints to trace
+
+        # Re-trace these fieldlines
+
+        # Setup the scene and plot fieldlines
+
+        # Animation additional frames if required
+
+def plot2d():
+    '''
+    Flattens data to a two-dimensional representation for a latitude-longitude projection plot of flux rope fieldlines.
+    '''
+
